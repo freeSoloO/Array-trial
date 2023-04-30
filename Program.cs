84fc85ea865daa302df05a0d31e72b4c98c4149e
Nikolay Georgiev <nikowhyy@gmail.com>
2023-04-30 13:10:12 +0300

    Array trial
    This is my first trial to push a code to GitHub.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CommonElements
+{
+    class CommonElements
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int fib = GetFibonacci(n);
+            Console.WriteLine(fib);
+        }
+        public static int GetFibonacci(int n)
+        {
+            if (n <= 2)
+            {
+                return 1;
+            }
+            return GetFibonacci(n - 1) + GetFibonacci(n - 2);
+        }
+        
+    }
+}
